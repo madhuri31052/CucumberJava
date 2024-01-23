b3e641fe279af2e68e1c11c802520d63f2794cd8
Matta Leela Madhuri <madhurimatta2000@gmail.com>
2024-01-23 16:25:51 +0530

    so many modifications

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -1,20 +1,22 @@
 package StepDefinitions;
 
-// To maintain many feature files we create this file
-
-import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features/", 
-glue= {"StepDefinitions"}, 
-monochrome = true,
-plugin = {"pretty", "junit:target/JUnitReports/reports.xml"}
-
+@CucumberOptions(
+    features = "src/test/resources/Features/",
+    glue = {"StepDefinitions"},
+    monochrome = true
+    // plugin = {"pretty", "junit:target/JUnitReports/reports.xml"}
 )
-
 public class TestRunner {
 
+    @BeforeClass
+    public static void setupWebDriver() {
+        // Initialize WebDriver using WebDriverManager
+        io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
+    }
 }
