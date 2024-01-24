b6ba9848481d1cb69e18c2e2e8a42aa7ecc5206f
Matta Leela Madhuri <madhurimatta2000@gmail.com>
2024-01-24 20:59:49 +0530

    tryy

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/Features/",
-    glue = {"StepDefinitions"},
+    glue = {"StepDefinitions", "POM_Pages"},
     monochrome = true,
     plugin = {"pretty", "html:target/cucumber-reports"}
 )
