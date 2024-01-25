86aeaaeeeeaf9713351ac9589f5a6f4fddc15218
Matta Leela Madhuri <madhurimatta2000@gmail.com>
2024-01-25 13:30:13 +0530

    modifications

--- src/test/java/POM_Pages/InstGoogleSteps_POM.java
+++ src/test/java/POM_Pages/InstGoogleSteps_POM.java
@@ -1,4 +1,4 @@
-package test.java.POM_Pages;
+package POM_Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
--- src/test/java/StepDefinitions/InstGoogleSteps_usingPOM.java
+++ src/test/java/StepDefinitions/InstGoogleSteps_usingPOM.java
@@ -1,4 +1,4 @@
-package test.java.StepDefinitions;
+package StepDefinitions;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
